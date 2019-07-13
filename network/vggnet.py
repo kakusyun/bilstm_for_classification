448a3791d28288b578a9b31ff7b738ebc32e824b
kakusyun <kakusyun@foxmail.com>
2019-07-13 20:48:20 +0800

    2019.7.13 vgg kernel_initializer add

--- network/vggnet.py
+++ network/vggnet.py
@@ -6,41 +6,41 @@ def VGG16(input_shape, classes):
     model = Sequential()
 
     # Block 1
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu',
-                     input_shape=input_shape, padding='same'))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_shape, padding='same',
+                     kernel_initializer='he_normal'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
     # Block 2
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
     # Block 3
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
     # Block 4
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
     # Block 5
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
     # 分类的全连接Top层
     model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096, activation='relu', kernel_initializer='he_normal'))
     model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096, activation='relu', kernel_initializer='he_normal'))
     model.add(Dropout(0.5))
-    model.add(Dense(classes, activation='softmax'))
+    model.add(Dense(classes, activation='softmax', kernel_initializer='he_normal'))
 
     return model
 
