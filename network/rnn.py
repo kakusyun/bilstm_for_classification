ace64b924bf3254f92df85fc6088257385f5fff6
kakusyun <kakusyun@foxmail.com>
2019-07-16 11:08:13 +0800

    2019.7.16 l2

--- network/rnn.py
+++ network/rnn.py
@@ -1,10 +1,12 @@
 from keras.layers import Bidirectional, TimeDistributed, CuDNNLSTM
+from keras.regularizers import l2
 
 
 def TimeDistributed_CuDNNLSTM(inputs, output_size, name, mode, sequences=True):
     x = TimeDistributed(Bidirectional(CuDNNLSTM(output_size,
                                                 return_sequences=sequences,
                                                 kernel_initializer='he_normal',
+                                                kernel_regularizer=l2(1e-4),
                                                 name=name), merge_mode=mode))(inputs)
     return x
 
@@ -13,5 +15,6 @@ def Bidirectional_CuDNNLSTM(inputs, output_size, name, mode, sequences=True):
     x = Bidirectional(CuDNNLSTM(output_size,
                                 return_sequences=sequences,
                                 kernel_initializer='he_normal',
+                                kernel_regularizer=l2(1e-4),
                                 name=name), merge_mode=mode)(inputs)
     return x
